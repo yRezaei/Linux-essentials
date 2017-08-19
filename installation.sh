c820ecd75ba78687c8850fe62bc55eb0520d4f4e
yRezaei <yashar.a.rezaei@gmail.com>
2017-08-19 21:32:31 +0200

    Minor correction

--- installation.sh
+++ installation.sh
@@ -23,4 +23,5 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # NVIDIA driver repository
-sudo add-apt-repository ppa:graphics-drivers/ppa && sudo apt update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update

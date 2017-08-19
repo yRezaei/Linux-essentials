3fab684e622c8a087bf3f75aa29cf67af9af985b
yRezaei <yashar.a.rezaei@gmail.com>
2017-08-20 01:21:47 +0200

    vscode installation command

--- installation.sh
+++ installation.sh
@@ -24,6 +24,8 @@ sudo apt-get install google-chrome-stable
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
 
 # NVIDIA driver repository
 sudo add-apt-repository ppa:graphics-drivers/ppa

eb2c763a2e588f7775d6a3f0f0a6d934f26873a9
yRezaei <yashar.a.rezaei@gmail.com>
2017-09-29 11:51:46 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -22,7 +22,7 @@ source ~/.bashrc
 # Google chrome repository
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/googlechrome.list'
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/googlechrome.list'
 
 sudo apt-get update
 
